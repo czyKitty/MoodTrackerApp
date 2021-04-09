f943d5a7f5b4db2f49ae57a6a34a67bd806a5b99
icyair100 <34012403+icyair100@users.noreply.github.com>
2021-04-09 17:10:14 -0400

    Added 2 more API calls to get the keywords + general sentiment.

--- app/src/main/java/com/example/moodtracker/ui/talk/TalkFragment.java
+++ app/src/main/java/com/example/moodtracker/ui/talk/TalkFragment.java
@@ -1,26 +1,37 @@
 package com.example.moodtracker.ui.talk;
 
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.moodtracker.R;
 import com.ibm.cloud.sdk.core.security.Authenticator;
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
 
+import com.ibm.watson.natural_language_understanding.v1.NaturalLanguageUnderstanding;
+import com.ibm.watson.natural_language_understanding.v1.model.AnalysisResults;
+import com.ibm.watson.natural_language_understanding.v1.model.AnalyzeOptions;
+import com.ibm.watson.natural_language_understanding.v1.model.Features;
+import com.ibm.watson.natural_language_understanding.v1.model.KeywordsOptions;
+import com.ibm.watson.natural_language_understanding.v1.model.SentimentOptions;
 import com.ibm.watson.tone_analyzer.v3.ToneAnalyzer;
 import com.ibm.watson.tone_analyzer.v3.model.ToneAnalysis;
 import com.ibm.watson.tone_analyzer.v3.model.ToneOptions;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 public class TalkFragment extends Fragment {
@@ -28,40 +39,120 @@ public class TalkFragment extends Fragment {
     private TalkViewModel dashboardViewModel;
     private final AtomicBoolean running = new AtomicBoolean(false);
     private String input;
-    private String sentiment;
     private Runnable mytask;
+    Map<String, Journal> journals = new HashMap<>();
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("server/saving-data/journals");
+    DatabaseReference journalsRef = ref.child("journals");
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 new ViewModelProvider(this).get(TalkViewModel.class);
         View root = inflater.inflate(R.layout.fragment_talk, container, false);
-//        input = edtInput.getText().toString();
-//        running.set(true);
-//        new Thread(mytask).start();
         return root;
     }
-    public void sentiment(String input) {
+
+    public class Journal {
+
+        public Date date;
+        public String text;
+        public AnalysisResults sentiment;
+        public AnalysisResults keywords;
+        public ToneAnalysis tones;
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        public Journal(String entry_text) {
+            // ...
+            text = entry_text;
+//            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            date = new Date();
+//            String dateString = formatter.format(date);
+            Analysis_nlp out = sentiment(text);
+            sentiment = out.sentiment;
+            keywords = out.keys;
+            tones = out.tone;
+        }
+
+        public Journal(String entry_text, String date) {
+            // ...
+        }
+    }
+
+    public void newEntry(String input) {
+        Map<String, Journal> journals = new HashMap<>();
+        Journal j = new Journal(input);
+        journals.put("Journals", j);
+        journalsRef.updateChildrenAsync(journals);
+    }
+
+    static class Analysis_nlp {
+        ToneAnalysis tone;
+        AnalysisResults keys;
+        AnalysisResults sentiment;
+        Analysis_nlp(ToneAnalysis a, AnalysisResults b, AnalysisResults c)
+        {
+            tone = a;
+            keys = b;
+            sentiment = c;
+        }
+    }
+
+    public Analysis_nlp sentiment(String input) {
+        AtomicReference<ToneAnalysis> tone = new AtomicReference<>(new ToneAnalysis());
+        AtomicReference<AnalysisResults> keys = new AtomicReference<>(new AnalysisResults());
+        AtomicReference<AnalysisResults> score= new AtomicReference<>(new AnalysisResults());
         //        Authenticate the API
         Authenticator authenticator = new IamAuthenticator("ZaupUklyab6ixv-73XYreQZLWOAfYv85DnjxUyklz4ry");
         ToneAnalyzer service = new ToneAnalyzer("2017-09-21", authenticator);
         service.setServiceUrl("https://api.us-east.tone-analyzer.watson.cloud.ibm.com/instances/c2f34cbf-a90a-4785-88d8-a41c0a08ac8c");
 
-//        Run Network API Call on seperate thread
-//        Use seperate thread so to not block the UI thread
+        Authenticator authenticator_nlp = new IamAuthenticator("SO_Deq8xw8V3uT1inqIAqzyMi2c7ku4OBhcwg97Nb5f6");
+        NaturalLanguageUnderstanding service_nlp = new NaturalLanguageUnderstanding("2019-07-12", authenticator_nlp);
+        service_nlp.setServiceUrl("https://api.us-east.natural-language-understanding.watson.cloud.ibm.com/instances/4dd4583a-125a-47b2-bec8-4bf171ffca01");
+
+//        Run Network API Call on separate thread
+//        Use separate thread so to not block the UI thread
         mytask = () -> {
-            while (running.get()){
                 try  {
-//        Build API Request
+                    // Tone Analysis
                     ToneOptions toneOptions = new ToneOptions.Builder()
                             .text(input)
                             .build();
-                    ToneAnalysis tone = service.tone(toneOptions).execute().getResult();
-                    sentiment = tone.toString();
-                    running.set(false);
+                    tone.set(service.tone(toneOptions).execute().getResult());
+                    // Keyword Analysis
+                    KeywordsOptions keywords = new KeywordsOptions.Builder()
+                            .sentiment(true)
+                            .emotion(true)
+                            .limit(3)
+                            .build();
+                    Features features = new Features.Builder()
+                            .keywords(keywords)
+                            .build();
+                    AnalyzeOptions parameters = new AnalyzeOptions.Builder()
+                            .text(input)
+                            .features(features)
+                            .build();
+                    keys.set(service_nlp.analyze(parameters).execute().getResult());
+                    // General Sentiment Analysis
+                    SentimentOptions sentiment = new SentimentOptions.Builder()
+                            .document(true)
+                            .build();
+                    Features features_2 = new Features.Builder()
+                            .sentiment(sentiment)
+                            .build();
+                    AnalyzeOptions parameters_2 = new AnalyzeOptions.Builder()
+                            .text(input)
+                            .features(features_2)
+                            .build();
+                    score.set(service_nlp.analyze(parameters_2).execute().getResult());
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }};
+            };
+        mytask.run();
+        return new Analysis_nlp(tone.get(), keys.get(), score.get());
     }
+
 }
