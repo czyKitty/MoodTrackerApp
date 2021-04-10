128fdc50f44b27e909374913c1ddef37288f8988
czyKitty <zchen98@bu.edu>
2021-04-10 16:59:39 -0400

    Add chart layout

--- app/src/main/java/com/example/moodtracker/ui/track/PieChartActivity.java
+++ app/src/main/java/com/example/moodtracker/ui/track/PieChartActivity.java
@@ -1,4 +1,79 @@
 package com.example.moodtracker.ui.track;
 
-public class PieChartActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.chart.common.listener.Event;
+import com.anychart.chart.common.listener.ListenersInterface;
+import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
+import com.example.moodtracker.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PieChartActivity extends AppCompatActivity {
+
+    ImageButton btnBack;
+    Spinner selectTime;
+    AnyChartView anyChartView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chart_layout);
+
+        //define view variables
+        anyChartView = findViewById(R.id.chartView);
+        anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+        btnBack = (ImageButton)findViewById(R.id.btn_back);
+        selectTime = (Spinner)findViewById(R.id.select_time);
+
+        //initialize spinner option.
+        String[] items = new String[]{"Past Week", "Past Month", "Past 3 Months"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        selectTime.setAdapter(adapter);
+
+        //back to track page
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PieChartActivity.this, TrackFragment.class));
+            }
+        });
+
+        Pie pie = AnyChart.pie();
+
+        // click display
+        pie.setOnClickListener(new ListenersInterface.OnClickListener(new String[]{"x", "value"}) {
+            @Override
+            public void onClick(Event event) {
+                Toast.makeText(PieChartActivity.this, event.getData().get("x") + ":" + event.getData().get("value"), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Read data from database
+        List<DataEntry> data = new ArrayList<>();
+        //data.add(new ValueDataEntry("Mood", value));
+
+        // set data
+        pie.data(data);
+
+        // place to view
+        anyChartView.setChart(pie);
+    }
+
 }
